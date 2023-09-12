ad01f1313acea5499313ef47aa2844cd21e4ccbe
Pedro Nascimento <sn.jaypee@gmail.com>
2023-09-11 22:18:22 -0300

    Aula 1, 2 e 3

---
+++ aula-ts-03/src/index.ts
@@ -0,0 +1,11 @@
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+// const num = 42;
+// const text = "teste";
+// const valid = false;
+
+const result = sum(1,2);
+console.log(result);
